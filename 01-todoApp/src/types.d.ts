5fd74c07f2d4f954fe83567710545f034b7340ae
Javier Flores <jffc.dev@gmail.com>
2023-03-05 21:37:30 -0500

    feat(App): TodoId, TodoTitle and TodoCompleted types added

--- 01-todoApp/src/types.d.ts
+++ 01-todoApp/src/types.d.ts
@@ -3,4 +3,9 @@ export interface TodoType {
   title: string
   completed: boolean
 }
+
+export type TodoId = Pick<Todo, "id">
+export type TodoTitle = Pick<Todo, "title">
+export type TodoCompleted = Pick<Todo, "completed">
+
 export type ListOfTodos = TodoType[]
