c15a1a8949a844fa8e89a036283deb231d93fb70
Javier Flores <jffc.dev@gmail.com>
2023-03-10 16:45:43 -0500

    feat(App): Footer props and content added

--- 01-todoApp/src/components/Footer.tsx
+++ 01-todoApp/src/components/Footer.tsx
@@ -1,7 +1,30 @@
-export const Footer: React.FC = () => {
+import { Filters } from "./Filters"
+import { type FilterValue } from "../types"
+
+interface Props {
+  activeCount: number
+  completedCount: number
+  filterSelected: FilterValue
+  onClearCompleted: () => void
+  handleFilterChange: (filter: FilterValue) => void
+}
+
+export const Footer: React.FC<Props> = ({
+  activeCount = 0,
+  completedCount = 0,
+  filterSelected,
+  handleFilterChange,
+  onClearCompleted,
+}) => {
   return (
     <footer className="footer">
-      <span className="todo-count"></span>
+      <span className="todo-count">
+        <strong>{activeCount} pending tasks</strong>
+      </span>
+      <Filters
+        filterSelected={filterSelected}
+        onFilterChange={handleFilterChange}
+      ></Filters>
     </footer>
   )
 }
