fbe7cc70bceac7c87fc20e10435e6505255bfa16
Javier Flores <jffc.dev@gmail.com>
2023-03-10 16:44:47 -0500

    feat(App): FilterValue type added

--- 01-todoApp/src/types.d.ts
+++ 01-todoApp/src/types.d.ts
@@ -1,3 +1,5 @@
+import { type TODO_FILTERS } from "./consts"
+
 export interface TodoType {
   id: string
   title: string
@@ -9,3 +11,4 @@ export type TodoTitle = Pick<Todo, "title">
 export type TodoCompleted = Pick<Todo, "completed">
 
 export type ListOfTodos = TodoType[]
+export type FilterValue = (typeof TODO_FILTERS)[keyof typeof TODO_FILTERS]
